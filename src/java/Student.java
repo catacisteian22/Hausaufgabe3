4e423b96a57c227708593f1fd1388f9da0bf68ff
Cătălina Cisteian <84501255+catacisteian22@users.noreply.github.com>
2021-10-19 17:43:56 +0300

    Initial commit

---
+++ src/java/Student.java
@@ -0,0 +1,14 @@
+package java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Student {
+
+    long studentId;
+    int totalCredits;
+    List<Course> enrolledCourses = new ArrayList<>();
+
+
+}
---
+++ src/java/Teacher.java
@@ -0,0 +1,8 @@
+package java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher {
+    List<Course> courses = new ArrayList<>();
+}
